42d846ecbd3322ba051f272882d7ef6f5154bf6e
sahil gupta <gsahil438@gmail.com>
2024-09-28 15:03:46 +0530

    Your

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,9 +16,7 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
-    );
+    const data = await fetch('/api/fetchData');
 
     const json = await data.json();
     console.log(json, 'json');
