28fbd1bf0a08b131c7f81a36ceb4122835456f3d
sahil gupta <gsahil438@gmail.com>
2024-10-26 19:54:41 +0530

    vercel1

--- api/swiggyProxy.js
+++ api/swiggyProxy.js
@@ -1,19 +1,15 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
-  // Define the Swiggy API endpoint you want to fetch data from
   const swiggyURL =
     'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
   try {
-    // Fetch data from the Swiggy API
+    // Fetch data directly from Swiggy API
     const response = await fetch(swiggyURL);
     const data = await response.json();
 
-    // Send the data back to the client
     res.status(200).json(data);
   } catch (error) {
-    // Handle errors by sending an error message to the client
+    console.error('Error fetching data from Swiggy:', error);
     res.status(500).json({ error: 'Error fetching data from Swiggy' });
   }
 }
